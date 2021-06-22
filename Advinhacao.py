b4732ef8ac8b916bbfade2cd8e5b02250aad733f
GhaSilva <ghabriel-silva@outlook.com>
2021-06-22 13:36:41 -0300

    Adicionando niveis de dificuldade

--- Advinhacao.py
+++ Advinhacao.py
@@ -5,8 +5,20 @@ print("Bem vindo ao jogo de Advinhação")
 print("*******************************")
 numero_random = random.randrange(1,101)
 numero_secreto = numero_random
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Sua escolha: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 
 
 for rodada in range(1, total_de_tentativas+1):
