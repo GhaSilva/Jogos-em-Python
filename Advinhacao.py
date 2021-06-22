eb8258df0af84f083b40ebf358b961540952c335
GhaSilva <ghabriel-silva@outlook.com>
2021-06-22 13:22:29 -0300

    Adicionando laço (for) e encerrando interação

--- Advinhacao.py
+++ Advinhacao.py
@@ -7,10 +7,11 @@ total_de_tentativas = 3
 rodada = 1
 
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas+1):
     print("Tentativa {} de {}" .format(rodada,total_de_tentativas))
     chute = int(input("Digite seu numero: "))
-
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
     print("Você digitou ", chute)
 
     acertou = numero_secreto == chute
