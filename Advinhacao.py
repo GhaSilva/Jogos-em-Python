1d194996f431856006baf881d0954c907edbea58
GhaSilva <ghabriel-silva@outlook.com>
2021-06-22 13:15:37 -0300

    Criando laço while para gerar tentativas

--- Advinhacao.py
+++ Advinhacao.py
@@ -3,20 +3,27 @@ print("Bem vindo ao jogo de Advinhação")
 print("*******************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite seu numero: "))
 
-print("Você digitou ", chute)
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}" .format(rodada,total_de_tentativas))
+    chute = int(input("Digite seu numero: "))
 
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    print("Você digitou ", chute)
 
-if(acertou):
-    print("Você acertou")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto")
-    elif (menor):
-        print("Você errou! O seu chute foi menor do que o número secreto")
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior do que o número secreto")
+        elif (menor):
+            print("Você errou! O seu chute foi menor do que o número secreto")
+        rodada += 1
 print("Fim de jogo")
