382d158d1286b9c90c8c9f5a60ca61669912fdec
GhaSilva <ghabriel-silva@outlook.com>
2021-06-24 18:10:40 -0300

    Criando game loop para jogo da forca

--- Forca.py
+++ Forca.py
@@ -3,6 +3,14 @@ def jogar():
     print("***Bem vindo ao jogo de Forca**")
     print("*******************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+    
+    while(not enforcou and not acertou):
+        print("Jogando...")
+
+
     print("Fim de jogo")
 if(__name__ == "__main__"):
     jogar()
