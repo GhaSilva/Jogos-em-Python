74e8f3965d43fc480dea4f3e343a86081d72688b
GhaSilva <ghabriel-silva@outlook.com>
2021-06-24 18:41:47 -0300

    Letras acertadas

--- Forca.py
+++ Forca.py
@@ -4,20 +4,23 @@ def jogar():
     print("*******************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ").strip()
+
+
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra.upper(),index))
+                letras_acertadas[index] = letra
             index += 1
+        print(letras_acertadas)
 
 
-        print("Jogando...")
 
 
     print("Fim de jogo")
